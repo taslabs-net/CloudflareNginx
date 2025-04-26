b10f6711e44ff54b4d66fc29c43b46e9a61c3435
Timothy Schneider <168775912+taslabs-net@users.noreply.github.com>
2025-04-26 17:23:33 -0400

    Update install_webhook.sh
    changed nginx
    location / {
            proxy_pass http://127.0.0.1:${PORT};
            proxy_http_version 1.1;
            proxy_set_header Host \$host;
            proxy_set_header X-Real-IP \$remote_addr;
            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
            proxy_set_header X-Forwarded-Proto \$scheme;
    
            proxy_set_header Upgrade $http_upgrade;
            proxy_set_header Connection "Upgrade";
    
            proxy_cache_bypass $http_upgrade;
            proxy_buffering off;
    
        }

--- install_webhook.sh
+++ install_webhook.sh
@@ -383,13 +383,17 @@ server {
     location / {
         proxy_pass http://127.0.0.1:${PORT};
         proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_cache_bypass \$http_upgrade;
+
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+
+        proxy_cache_bypass $http_upgrade;
+        proxy_buffering off;
+
     }
 }
 EOF
